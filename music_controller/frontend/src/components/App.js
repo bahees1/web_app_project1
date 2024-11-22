e44a788f1738d408dd1c00eda7b53973eb42bd25
bahees1 <144285701+bahees1@users.noreply.github.com>
2024-11-22 15:42:54 -0500

    learned about routing and created different pages

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import { RedeemRounded } from "@material-ui/icons";
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props) {
@@ -8,7 +9,10 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React code</h1>;
+        return (<div>
+            <HomePage />
+            </div>
+        );
     }
 }
 
